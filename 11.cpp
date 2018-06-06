ba7026c8a18e9394b300fe11748ad41e7b62a5c8
KiraNetyksha <kyrandia.ire@gmail.com>
2018-06-06 12:23:26 +0300

    wasdfg

---
+++ 11.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+main (void)
+{
+ int k=0, j=0;
+ char str[50];
+
+ puts ("Vvedite Vash text:");
+ gets (str);
+ puts ("Kolichestvo bukv 'i' v texte sledujushee:");
+ while (str[k] != NULL)
+ {
+  if (str[k]=='i')
+  j++;
+  k++;
+ }
+ printf ("%d", j);
+
+getch ();
+return 0;
+}
